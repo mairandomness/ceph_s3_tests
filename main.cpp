2a64201e16664c04ef054e2a88c0bd29ce27c918
mairandomness <mairakodama@gmail.com>
2018-03-19 23:28:26 -0300

    S3 client boilerplate

--- main.cpp
+++ main.cpp
@@ -10,5 +10,21 @@ int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     RUN_ALL_TESTS();
     std::cout << "Hello, World!" << std::endl;
+
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
+
+    Aws::Client::ClientConfiguration clientconfig;
+    clientconfig.region = "mexico";
+    clientconfig.scheme = Aws::Http::Scheme::HTTP;
+    std::string endpoint = "localhost:8000";
+    clientconfig.endpointOverride = endpoint.c_str();
+
+    Aws::S3::S3Client s3_client(clientconfig);
+
+    // the tests
+
+
+    Aws::ShutdownAPI(options);
     return 0;
 }
