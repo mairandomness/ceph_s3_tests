f5845d413de156f2ee35408547fab7f11d8599a8
mairandomness <mairakodama@gmail.com>
2018-03-19 23:00:15 -0300

    Add aws-sdk-cpp to the project
    I had to bump CXX_STANDARD and CPP_STANDARD to 17 bc of this:
    
    https://github.com/aws/aws-sdk-cpp/issues/620

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <aws/core/Aws.h>
+#include <aws/s3/S3Client.h>
+#include <aws/s3/model/Bucket.h>
+#include <aws/s3/model/CreateBucketRequest.h>
 
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
